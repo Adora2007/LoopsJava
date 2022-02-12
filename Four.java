c167efe6578f9a9c005f713c207909e8c149a19c
Adora2007 <81971875+Adora2007@users.noreply.github.com>
2022-02-12 11:22:14 +0530

    Add files via upload

---
+++ Four.java
@@ -0,0 +1,23 @@
+public class Four {
+    public static void main(String args[]) {
+  //int i =0;
+  //while(i<0){
+      //System.out.println(i);
+      //i = i+1;
+     //} i = 0;
+
+     int i = 12;
+   do{
+  System.out.println("Apna Collge");
+   } while( i<11);
+
+} 
+}
+//Notes
+// counter++ is called increment operator;
+//you can also write counter+1;
+//anaylsing the code without writing is called dry run;
+//there are 3 stages in a for loop(intialisation, termination, updation);
+//It checks and updates;
+//while condition means while doing;
+// in do while even if it is true or false it will work for atleast 1;
